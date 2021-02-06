5ee3bdd98d749e97785b257fdbf77a9735c95990
Nicolas94340 <58393647+Nicolas94340@users.noreply.github.com>
2021-02-06 11:31:31 +0100

    TP spring Boot

---
+++ tp-teams-handling-app/src/app/app.module.ts
@@ -0,0 +1,26 @@
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import {TeamsModule} from "@team-handling/teams/teams.module";
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import {MatToolbarModule} from "@angular/material/toolbar";
+import {HttpClientModule} from "@angular/common/http";
+
+@NgModule({
+  declarations: [
+    AppComponent
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule,
+    TeamsModule,
+    BrowserAnimationsModule,
+    MatToolbarModule,
+    HttpClientModule,
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
